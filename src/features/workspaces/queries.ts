3ec81b625f1d7d6722db3a469d9ec958324dfd85
ParthaPradeepNath <parthanath96134@gmail.com>
2025-09-18 16:04:51 +0530

    project setting done

--- src/features/workspaces/queries.ts
+++ src/features/workspaces/queries.ts
@@ -6,44 +6,38 @@ import { Workspace } from "./types";
 import { createSessionClient } from "@/lib/appwrite";
 
 export const getWorkspaces = async () => {
-  try {
-    // const client = new Client()
-    //   .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-    //   .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+  // const client = new Client()
+  //   .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+  //   .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
 
-    // const session = (await cookies()).get(AUTH_COOKIE);
+  // const session = (await cookies()).get(AUTH_COOKIE);
 
-    // if (!session) return { documents: [], total: 0 };
+  // if (!session) return { documents: [], total: 0 };
 
-    // client.setSession(session.value);
+  // client.setSession(session.value);
 
-    // const databases = new Databases(client);
-    // const account = new Account(client);
+  // const databases = new Databases(client);
+  // const account = new Account(client);
 
-    const { databases, account } = await createSessionClient();
-    const user = await account.get();
+  const { databases, account } = await createSessionClient();
+  const user = await account.get();
 
-    const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
-      Query.equal("userId", user.$id),
-    ]);
+  const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [
+    Query.equal("userId", user.$id),
+  ]);
 
-    if (members.total === 0) {
-      return { documents: [], total: 0 };
-    }
-
-    const workspaceIds = members.documents.map((member) => member.workspaceId);
-
-    const workspaces = await databases.listDocuments(
-      DATABASE_ID,
-      WORKSPACES_ID,
-      [Query.orderDesc("$createdAt"), Query.contains("$id", workspaceIds)]
-    );
-
-    return workspaces;
-  } catch {
+  if (members.total === 0) {
     return { documents: [], total: 0 };
   }
-  // cant use re direct inside try-catch in NEXTJS
+
+  const workspaceIds = members.documents.map((member) => member.workspaceId);
+
+  const workspaces = await databases.listDocuments(DATABASE_ID, WORKSPACES_ID, [
+    Query.orderDesc("$createdAt"),
+    Query.contains("$id", workspaceIds),
+  ]);
+
+  return workspaces;
 };
 
 interface GetWorkspaceProps {
@@ -51,66 +45,58 @@ interface GetWorkspaceProps {
 }
 
 export const getWorkspace = async ({ workspaceId }: GetWorkspaceProps) => {
-  try {
-    // const client = new Client()
-    //   .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-    //   .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+  // const client = new Client()
+  //   .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+  //   .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
 
-    // const session = (await cookies()).get(AUTH_COOKIE);
+  // const session = (await cookies()).get(AUTH_COOKIE);
 
-    // if (!session) return null;
+  // if (!session) return null;
 
-    // client.setSession(session.value);
+  // client.setSession(session.value);
 
-    // const databases = new Databases(client);
-    // const account = new Account(client);
+  // const databases = new Databases(client);
+  // const account = new Account(client);
 
-    const { databases, account } = await createSessionClient();
+  const { databases, account } = await createSessionClient();
 
-    const user = await account.get();
+  const user = await account.get();
 
-    const member = await getMember({
-      databases,
-      userId: user.$id,
-      workspaceId,
-    });
+  const member = await getMember({
+    databases,
+    userId: user.$id,
+    workspaceId,
+  });
 
-    if (!member) {
-      return null;
-    }
-
-    const workspace = await databases.getDocument<Workspace>(
-      DATABASE_ID,
-      WORKSPACES_ID,
-      workspaceId
-    );
-
-    return workspace;
-  } catch {
-    return null;
+  if (!member) {
+    throw new Error("Unauthorized");
   }
-  // cant use re direct inside try-catch in NEXTJS
-};
 
+  const workspace = await databases.getDocument<Workspace>(
+    DATABASE_ID,
+    WORKSPACES_ID,
+    workspaceId
+  );
+
+  return workspace;
+};
 
 interface GetWorkspaceInfoProps {
   workspaceId: string;
 }
 
-export const getWorkspaceInfo = async ({ workspaceId }: GetWorkspaceInfoProps) => {
-  try {
-    const { databases } = await createSessionClient();
+export const getWorkspaceInfo = async ({
+  workspaceId,
+}: GetWorkspaceInfoProps) => {
+  const { databases } = await createSessionClient();
 
-    const workspace = await databases.getDocument<Workspace>(
-      DATABASE_ID,
-      WORKSPACES_ID,
-      workspaceId
-    );
+  const workspace = await databases.getDocument<Workspace>(
+    DATABASE_ID,
+    WORKSPACES_ID,
+    workspaceId
+  );
 
-    return {
-      name: workspace.name,
-    }
-  } catch {
-    return null;
-  }
+  return {
+    name: workspace.name,
+  };
 };
