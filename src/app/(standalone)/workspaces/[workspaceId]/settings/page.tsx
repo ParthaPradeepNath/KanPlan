17e5e96d2fe17776ee7eb8003f9baff1b0c29b9f
ParthaPradeepNath <parthanath96134@gmail.com>
2025-09-20 18:49:49 +0530

    worspace server comp done

--- src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
@@ -1,30 +1,16 @@
 import { redirect } from "next/navigation";
 
 import { getCurrent } from "@/features/auth/queries";
-import { EditWorkspaceForm } from "@/features/workspaces/components/edit-workspace-form";
-import { getWorkspace } from "@/features/workspaces/queries";
+import { WorkspaceIdSettingsClient } from "./client";
 
-interface WorkspaceIdSettingsPageProps {
-  params: {
-    workspaceId: string;
-  };
-}
 
-const WorkspaceIdSettingPage = async ({
-  params,
-}: WorkspaceIdSettingsPageProps) => {
+const WorkspaceIdSettingPage = async () => {
   const user = await getCurrent();
   if (!user) {
     redirect("/sign-in");
   }
 
-  const initialValues = await getWorkspace({ workspaceId: params.workspaceId });
-
-  return (
-    <div className="w-full lg:max-w-xl">
-      <EditWorkspaceForm initialValues={initialValues} />
-    </div>
-  );
+  return <WorkspaceIdSettingsClient />
 };
 
 export default WorkspaceIdSettingPage;
